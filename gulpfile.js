eda3ce6b825ec17a19b22d9ef889e20de97e360b
Robby Ingebretsen <robby@thinkpixellab.com>
2022-01-05 15:53:31 -0800

    Add testing infrastructure and move documentation output to /pages folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,36 @@
-const gulp = require('gulp');
 const watch = require('gulp-watch');
-const sass = require('gulp-sass')(require('sass'));
+
 const sassdoc = require('sassdoc');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-// ================================================================================
-// sassdoc
-// ================================================================================
-
-const runSassDoc = function () {
+// compile documentation with sassdoc
+function sassDoc(done) {
     gulp.src('./src/**/*.scss').pipe(sassdoc());
     //gulp.src('./src/defaults.scss').pipe(sassdoc());
-};
+    done();
+}
 
-const copyDocFiles = function () {
-    gulp.src(['./docs/src/index.html', './docs/data/docs.js']).pipe(gulp.dest('./docs/dist/'));
-};
-
-const buildStyles = function () {
+// build documentation styles
+function docStyles(done) {
     gulp.src('./docs/src/styles.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./docs/dist'));
+        .pipe(gulp.dest('./pages'));
+    done();
+}
+
+// copy documentation files to the pages folder
+function copyDocFiles(done) {
+    gulp.src(['./docs/src/index.html', './docs/data/docs.js']).pipe(gulp.dest('./pages/'));
+    done();
+}
+
+exports.sassDoc = sassDoc;
+exports.docStyles = docStyles;
+exports.copyDocFiles = copyDocFiles;
+exports.docs = gulp.series(sassDoc, docStyles, copyDocFiles);
+
+exports.default = function () {
+    let docWatch = ['./docs/src/styles.scss', './docs/theme/*.*', './docs/src/*.*', './**/*.scss'];
+    gulp.watch(docWatch, gulp.series(sassDoc, docStyles, copyDocFiles));
 };
-
-gulp.task('sassdoc', function (cb) {
-    runSassDoc();
-});
-
-gulp.task('styles', function (cb) {
-    buildStyles();
-    cb();
-});
-
-gulp.task('docs', function (cb) {
-    runSassDoc();
-    setTimeout(() => {
-        buildStyles();
-        copyDocFiles();
-        cb();
-    }, 1000);
-});
-
-// ================================================================================
-// watch
-// ================================================================================
-gulp.task('watch', function () {
-    gulp.watch(
-        ['./docs/src/styles.scss', './docs/theme/*.*', './docs/src/*.*', './**/*.scss'],
-        gulp.series(['docs', 'styles'])
-    );
-});
