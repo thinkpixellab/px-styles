4c7fe88f5a1a91d12c1a97147f04dd8e56433e78
Robby Ingebretsen <robby@thinkpixellab.com>
2024-04-25 09:53:20 -0700

    Script improvements

--- scratch/build.js
+++ scratch/build.js
@@ -4,9 +4,34 @@ const path = require('path');
 
 const SCSS_SOURCE_PATH = path.resolve(__dirname, './scratch.scss');
 const SCSS_OUTPUT_PATH = path.resolve(__dirname, './scratch.css');
+const DEFAULT_SOURCE = `
+@use "../all.scss" as *;
 
-const sassResult = sass.compile(SCSS_SOURCE_PATH);
+@include config('colors.page-bg', gold);
+@include boilerplate();
+`;
 
-if (sassResult.css) {
-    fs.writeFileSync(SCSS_OUTPUT_PATH, sassResult.css.toString());
-}
+const buildSass = () => {
+    const sassResult = sass.compile(SCSS_SOURCE_PATH);
+
+    if (sassResult.css) {
+        fs.writeFileSync(SCSS_OUTPUT_PATH, sassResult.css.toString());
+    }
+};
+
+// create scratch.scss if it doesn't exist
+fs.access(SCSS_SOURCE_PATH, fs.constants.F_OK, err => {
+    if (err) {
+        // File doesn't exist, create it
+        fs.writeFileSync(SCSS_SOURCE_PATH, DEFAULT_SOURCE, err => {
+            if (err) {
+                console.error('Error creating scratch.scss:', err);
+            } else {
+                console.log('scratch.scss created successfully:', SCSS_SOURCE_PATH);
+            }
+        });
+        buildSass();
+    } else {
+        buildSass();
+    }
+});
--- scratch/open.js
+++ scratch/open.js
@@ -1,5 +1,40 @@
+const fs = require('fs');
 const path = require('path');
-const INDEX_PATH = 'file://' + path.resolve(__dirname, './index.html');
+const INDEX_PATH = path.resolve(__dirname, './index.html');
 const opn = require('better-opn');
+console.log(`INDEX_PATH: ${INDEX_PATH}`);
 
-opn(INDEX_PATH);
+const DEFAULT_INDEX = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>px-styles Scratch!</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="" />
+        <link rel="stylesheet" type="text/css" href="scratch.css" />
+    </head>
+    <body>
+        <!-- INSERT HTML FOR TESTING -->
+        <div>TEST</div>
+    </body>
+</html>`;
+
+// create index.html if it doesn't exist
+fs.access(INDEX_PATH, fs.constants.F_OK, err => {
+    if (err) {
+        fs.writeFileSync(INDEX_PATH, DEFAULT_INDEX, err => {
+            if (err) {
+                console.error('Error creating index.html:', err);
+            } else {
+                console.log('index.html created successfully:', INDEX_PATH);
+            }
+        });
+
+        // if the file hasn't been created yet, we wait a second before loading it (this is actually
+        // so that scratch.scss can be built first and the first load reflects the current css)
+        opn('file://' + INDEX_PATH);
+    } else {
+        opn('file://' + INDEX_PATH);
+    }
+});
